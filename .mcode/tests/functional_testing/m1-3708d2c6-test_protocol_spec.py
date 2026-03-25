40c87403c3351d0ac933c42c1091f48fad246473
SandboxCache <sandbox@cache>
2026-03-25 13:23:56 +0000

    Apply cached changes

--- .mcode/tests/functional_testing/m1-3708d2c6-test_protocol_spec.py
+++ .mcode/tests/functional_testing/m1-3708d2c6-test_protocol_spec.py
@@ -9,7 +9,7 @@ This script supports two modes:
 1. SRC Validation: Tests commands and captures outputs (no expected_stdout/stderr)
 2. DST Contract Validation: Tests commands and validates outputs match expected
 
-Generated at: 2026-03-25T13:23:41.567000+00:00
+Generated at: 2026-03-25T13:07:26.085851+00:00
 Project: email-slicer-python-guy
 Milestone: 1
 """
